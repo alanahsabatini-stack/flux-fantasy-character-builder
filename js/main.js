eba8812196403964aef49c3a86be89d8c82efce0
Alanah Sabatini <alanahsabatini@gmail.com>
2026-08-08 14:00:17 -0400

    Add first JavaScript interaction

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log("Flux Fantasy Character Builder loaded!")
+
+const message = document.getElementById("message");
+const testButton = document.getElementById("testButton");
+
+testButton.addEventListener("click", function() {
+    message.textContent = "Welcome to the Flux Fantasy Character Builder!"
+});
